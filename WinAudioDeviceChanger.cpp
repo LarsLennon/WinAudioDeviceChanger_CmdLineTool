111fdd96b786884e15cb98a21e2133eefa765659
Lars Jensen <L@rslennon.dk>
2023-10-11 00:10:43 +0200

    Added console line arguments

--- WinAudioDeviceChanger.cpp
+++ WinAudioDeviceChanger.cpp
@@ -2,10 +2,57 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void DisplayHelp() {
+    std::cout << "AudioConsoleApp Help:" << std::endl;
+    std::cout << "  --help                      - Displays this help message." << std::endl;
+    std::cout << "  --version                   - Displays the version information." << std::endl;
+    std::cout << "  --setoutput [device name]   - Sets the output device to the specified device name." << std::endl;
+}
+
+void SetOutputDevice(const std::string& deviceName) {
+    if (deviceName.empty()) {
+        std::cout << "Error: Invalid device name." << std::endl;
+        return;
+    }
+
+    // Code to set the output device using the deviceName goes here.
+    std::cout << "Setting output to: " << deviceName << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        DisplayHelp();
+        return 0;
+    }
+
+    std::string command = argv[1];
+    if (command == "--help" || command == "-h") {
+        DisplayHelp();
+    }
+    else if (command == "--version" || command == "-v") {
+        std::cout << "WinAudioDeviceChanger Version 0.1.0" << std::endl;
+    }
+    else if (command == "--setoutput") {
+        if (argc > 2) {
+            std::stringstream ss;
+            for (int i = 2; i < argc; ++i) {
+                if (i != 2) ss << " ";
+                ss << argv[i];
+            }
+            SetOutputDevice(ss.str());
+        }
+        else {
+            std::cout << "Error: No device name specified." << std::endl;
+        }
+    }
+    else {
+        std::cout << "Unknown command. Type '--help' for available commands." << std::endl;
+    }
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
